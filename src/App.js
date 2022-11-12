f018d8449381200e6e7682ab6b871ed196b5a28a
DavidEcklund <davidecklund@protonmail.com>
2022-11-12 18:38:29 +0100

    Wrapping two elements in nothing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function App() {
-  return "Hello, World!"
+  return (
+    <>
+      <h1>
+        Hello, World!
+      </h1>
+      <h3>Subtitle</h3>
+    </>
+  )
 }
 
 export default App;
