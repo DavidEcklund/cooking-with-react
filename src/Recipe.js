51ef4699ea6af2c6babc42cb440e71a8337016d3
DavidEcklund <davidecklund@protonmail.com>
2022-11-13 20:03:55 +0100

    Create IngredientList and Ingredient and add Recipe

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import IngredientList from './IngredientList'
 
 export default function Recipe(props) {
   const {
     name,
     cookTime,
     servings,
-    instructions
+    instructions,
+    ingredients
   } = props
   
   return (
@@ -23,11 +25,17 @@ export default function Recipe(props) {
       </div>
       <div>
         <span>Servings:</span>
-        <span>{servings}}</span>
+        <span>{servings}</span>
       </div>
       <div>
         <span>Instructions:</span>
         <div>{instructions}</div>
+      </div>
+      <div>
+        <span>Ingredients:</span>
+        <div>
+          <IngredientList ingredients={ingredients} />
+        </div>
       </div>
     </div>
   )
